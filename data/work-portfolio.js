aaaeca5b5ba665e144a221ea9f8af64b4fb980fb
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2024-10-12 20:42:57 +0100

    Add: Work porfolio section

---
+++ data/work-portfolio.js
@@ -0,0 +1,16 @@
+export const workPortfolioCards = [{
+    title: "11 - Flair - Drone Mangement Dashboard",
+    image: "/images/CardImage/flair-dashboard.jpg",
+    description: "Creating a dashboard for drone delivery services to manage their drone fleets."
+},
+{
+    title: "10: - Konfhub - Ticketing / Checkout page",
+    image: "/images/CardImage/konfhub-checkoutPage.png",
+    description: "A ticket purchasing platform with complex features; More than 500,000 tickets sold and used by 100+ events"
+},
+{
+    title: "09: - Konfhub - Check-in App",
+    image: "/images/CardImage/konfhub-checkinapp.jpg",
+    description: "A PWA app that allows to check-in ticket holders, seamlessly works offline and online"
+},
+]
